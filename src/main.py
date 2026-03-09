662b5c7a54c9bcf6c0b1ee95ad23ba4927408976
Bo Dowling <bo.dowling@gmail.com>
2026-03-08 23:16:56 -0400

    Setup for GitHub Pages

--- src/main.py
+++ src/main.py
@@ -1,10 +1,49 @@
+import os
+import shutil
+import sys
+
 import site_generator
 
 
+def cp_folder(path: str, new_path: str) -> None:
+    if os.path.exists(new_path):
+        shutil.rmtree(new_path)
+    os.mkdir(new_path)
+    for item in os.listdir(path):
+        if os.path.isfile(os.path.join(path, item)):
+            shutil.copy(os.path.join(path, item), new_path)
+        else:
+            cp_folder(os.path.join(path, item), os.path.join(new_path, item))
+
+
+def generate_pages_recursive(
+    base_path: str, content_path: str, template_path: str, dest_dir_path: str
+) -> None:
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+    for item in os.scandir(content_path):
+        if item.is_dir():
+            generate_pages_recursive(
+                base_path,
+                item.path,
+                template_path,
+                os.path.join(dest_dir_path, item.name),
+            )
+        elif item.name.endswith(".md"):
+            html_name = os.path.splitext(item.name)[0] + ".html"
+            site_generator.generate_page(
+                base_path,
+                item.path,
+                template_path,
+                os.path.join(dest_dir_path, html_name),
+            )
+
+
 def main():
-    site_generator.generate_page(
-        "content/index.md", "template.html", "public/index.html"
-    )
+    args = sys.argv
+    base_path = args[0] if len(args) > 0 else "/"
+    cp_folder("static", "docs")
+    generate_pages_recursive(base_path, "content", "template.html", "public")
 
 
 if __name__ == "__main__":
--- src/site_generator/site_generator.py
+++ src/site_generator/site_generator.py
@@ -9,7 +9,9 @@ def extract_title(md: str) -> str:
     raise ValueError("No header found")
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_page(
+    base_path: str, from_path: str, template_path: str, dest_path: str
+) -> None:
     print(f"Generating {dest_path} from {from_path} with template {template_path}")
     with open(from_path, "r") as f:
         markdown = f.read()
@@ -18,6 +20,11 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
 
     html = md_to_html_node(markdown).to_html()
     title = extract_title(markdown)
-    page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    page = (
+        template.replace("{{ Title }}", title)
+        .replace("{{ Content }}", html)
+        .replace('href="/', f'href="{base_path}')
+        .replace('src="/', f'src="{base_path}')
+    )
     with open(dest_path, "w") as f:
         f.write(page)
